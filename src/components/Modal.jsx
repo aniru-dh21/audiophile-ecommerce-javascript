e4abafad37db93e4b8ccd3bc0662ee6db9f86955
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-13 00:52:35 +0530

    Modal Component added

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import PropType from "prop-types";
+import "../styles/modal.css";
+import ReactDom from "react-dom";
+
+function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null;
+
+  return ReactDom.createPortal(
+    <div onClick={onClose} className="modal-overlay">
+      <div onClick={(e) => e.stopPropagation()} className="modal-content">
+        {children}
+      </div>
+    </div>,
+    document.getElementById("portal")
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropType.bool.isRequired,
+  onClose: PropType.func.isRequired,
+  children: PropType.node.isRequired,
+};
+
+export default Modal;
