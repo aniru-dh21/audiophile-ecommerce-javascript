b4cc4c7cc62aee244dcc8cdd242b8a301edeb21b
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-13 00:05:54 +0530

    NavMenu Component Added

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import hamburger from "../assets/shared/mobile/icon-hamburger.svg";
+import { IoMdClose } from "react-icons/io";
+import Menu from "./Menu";
+import PropType from "prop-types";
+
+function NavMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleMenu() {
+    setIsOpen((prev) => !prev);
+  }
+
+  function menuClose() {
+    setIsOpen(false);
+  }
+
+  return (
+    <>
+      <button onClick={toggleMenu} className="menu-toggle">
+        {isOpen ? (
+          <IoMdClose className="close-icon" />
+        ) : (
+          <img src={hamburger} alt="" />
+        )}
+      </button>
+
+      {isOpen && (
+        <div className="nav-menu">
+          <Menu menuClose={menuClose} />
+        </div>
+      )}
+    </>
+  );
+}
+
+NavMenu.propTypes = {
+  isOpen: PropType.bool,
+};
+
+export default NavMenu;
