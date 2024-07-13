ee82c00d09cbf7a13a53e7e898dd4c0624bb0298
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-13 16:27:09 +0530

    Header Component Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import PropType from "prop-types";
+import "../styles/header.css";
+
+function Header({ children }) {
+  return (
+    <header className="header">
+      <h2 className="heading-2 page-heading">{children}</h2>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  children: PropType.string,
+};
+
+export default Header;
