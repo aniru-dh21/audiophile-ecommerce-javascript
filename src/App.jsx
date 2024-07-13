5ca57fe43465a42fce003e6bf2fe225b5df51c8b
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-13 16:29:47 +0530

    Modified App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path=":category" element={<Category />} />
         </Route>
       </Routes>
     </BrowserRouter>
