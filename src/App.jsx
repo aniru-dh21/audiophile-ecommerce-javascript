db9ec08d13f9153e2cc12c207ba2633a8cf2dcf7
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-13 14:37:25 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./utils/ScrollToTop";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import "../styles/home.css";
 import ProductHeadphones from "../components/ProductHeadphones";
-import Menu from "../components/Menu";
 import ProductSpeaker from "../components/ProductSpeaker";
 import ProductEarphones from "../components/ProductEarphones";
+import Menu from "../components/Menu";
 import About from "../components/About";
 
 function Home() {
