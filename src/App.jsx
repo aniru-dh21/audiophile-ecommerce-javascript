85aa2c454f1336e9cbaf61da49993328bb04a20c
Ramachandra Anirudh Vemulapalli <vemulapallianirudh18@gmail.com>
2024-07-16 22:48:14 +0530

    App Modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ScrollToTop from "./utils/ScrollToTop";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path=":category" element={<Category />} />
+          <Route path=":category/:productId" element={<ProductDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
